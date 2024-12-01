364ce3e449d07f74d6f0177fcabb804a5c902b80
congtrung6391 <congtrung6391@gmail.com>
2024-12-01 13:55:42 +0700

    fix type

--- keystone-context.ts
+++ keystone-context.ts
@@ -1,10 +1,9 @@
 import { getContext } from '@keystone-6/core/context';
 import config from './keystone';
-import { Context } from '.keystone/types';
 import * as PrismaModule from '.prisma/client';
 
 // Making sure multiple prisma clients are not created during dev hot reloading
-export const keystoneContext: Context =
+export const keystoneContext =
   (globalThis as any).keystoneContext || getContext(config, PrismaModule);
 
 if (process.env.NODE_ENV !== 'production') {
