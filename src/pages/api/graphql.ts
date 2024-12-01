700e3e43e67a7b46337001ec7356075ed5c33a08
congtrung6391 <congtrung6391@gmail.com>
2024-12-01 13:49:50 +0700

    update

--- src/pages/api/graphql.ts
+++ src/pages/api/graphql.ts
@@ -1,6 +1,6 @@
 import { ApolloServer } from "@apollo/server";
-import { keystoneContext } from "../../keystone-context";
 import { startServerAndCreateNextHandler } from "@as-integrations/next";
+import { keystoneContext } from "../../../keystone-context";
 
 const apolloServer = new ApolloServer({
   schema: keystoneContext.graphql.schema
